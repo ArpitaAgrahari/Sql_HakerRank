b216165b546be1037daa2a441ee48d34357fa159
Arpita Agrahari <115877354+ArpitaAgrahari@users.noreply.github.com>
2023-04-18 22:37:01 +0530

    Add files via upload

---
+++ Weather Observation Station 17 in SQL.sql
@@ -0,0 +1,7 @@
+/*  Weather Observation Station 17 in SQL | HackerRank Solution  */
+
+
+
+select round(long_w, 4) from station 
+where lat_n = (select min(lat_n) 
+from station where lat_n > 38.7780);
