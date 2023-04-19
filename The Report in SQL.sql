5617f0ba96080a5b8fbda08f47571098944f159a
Arpita Agrahari <115877354+ArpitaAgrahari@users.noreply.github.com>
2023-04-19 23:19:48 +0530

    Add files via upload

---
+++ The Report in SQL.sql
@@ -0,0 +1,16 @@
+/*  The Report in SQL | HackerRank Solution  */
+
+
+
+SELECT CASE
+    WHEN G.grade > 7 THEN S.name
+    ELSE NULL
+    end AS names,
+    G.grade,
+    S.marks
+FROM   students S
+    JOIN grades G
+    ON S.marks BETWEEN G.min_mark AND G.max_mark
+ORDER  BY G.grade DESC,
+    names ASC,
+    S.marks ASC;
